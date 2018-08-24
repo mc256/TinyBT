ad0ad723d2ff997ff836be9a6d35dbfb62b29989
mc1024 <contact@masterchan.me>
2018-08-24 12:02:48 -0400

    Initial Commit

---
+++ serika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  serika
+//
+//  Created by maverick on 2018/8/24.
+//  Copyright © 2018 maverick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
